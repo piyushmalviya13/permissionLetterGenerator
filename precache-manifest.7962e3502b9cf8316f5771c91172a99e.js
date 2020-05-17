d28ba053b33d9cea627c19ca71a82c41f794c50f
Piyush <mpiyush484@gmail.com>
2020-05-17 17:27:42 +0530

    Updates

---
+++ precache-manifest.7962e3502b9cf8316f5771c91172a99e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49a8c8308705bd89391c9b233091ea9",
+    "url": "/permissionLetterGenerator/index.html"
+  },
+  {
+    "revision": "cf988b5986314a7e18ed",
+    "url": "/permissionLetterGenerator/static/css/2.55c508d7.chunk.css"
+  },
+  {
+    "revision": "e3c4933e42c857a0e672",
+    "url": "/permissionLetterGenerator/static/css/main.7b969e4c.chunk.css"
+  },
+  {
+    "revision": "cf988b5986314a7e18ed",
+    "url": "/permissionLetterGenerator/static/js/2.e0b68adb.chunk.js"
+  },
+  {
+    "revision": "40f6ddb379fd6d5397eded99d3b7f8f9",
+    "url": "/permissionLetterGenerator/static/js/2.e0b68adb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c4933e42c857a0e672",
+    "url": "/permissionLetterGenerator/static/js/main.7d4d2175.chunk.js"
+  },
+  {
+    "revision": "81d32e27c8f2e1caed34",
+    "url": "/permissionLetterGenerator/static/js/runtime-main.7d750808.js"
+  }
+]);
